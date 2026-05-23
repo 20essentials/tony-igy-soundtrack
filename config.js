036a34a0fa0e75bc5ac1c2aa89fae3ad8a2f8843
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:02:58 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'TONY IGY';
+export let nameSongs = [
+	"ASTRONOMIA", /*1*/
+	"PENTAGRAMMA", /*2*/
+	"M.E.D.U.Z.Z.A.", /*3*/
+	"THE HEAT", /*4*/
+	"DREAMWALKER", /*5*/
+	"PLAYING", /*6*/
+	"OPEN FIRE", /*7*/
+	"ASTRONOMIA (NEVER GO HOME)", /*8*/
+	"STREET SADNESS", /*9*/
+	"CHANGE", /*10*/
+	"BECAUSE OF YOU", /*11*/
+	"CIVIK", /*12*/
+	"LET'S RUN", /*13*/
+	"ORPHA", /*14*/
+	"HOT", /*15*/
+	"EVOLUTION THEORY", /*16*/
+	"BREATHE ME SEA", /*17*/
+	"SHOW YOU HOW", /*18*/
+	"TAKE IT TO THE TOP", /*19*/
+	"MELODRAMATIK", /*20*/
+]
